f9db4fe0cfb25a493a99c159e8d4aa2ac4684559
Mikołaj Nowakowski <mikolajnowakowski99@gmail.com>
2024-04-19 13:20:51 +0200

    Initial commit

---
+++ src/main/java/com/app/SpringBootSlenieumCucumberProj/SpringBootSlenieumCucumberProjApplication.java
@@ -0,0 +1,13 @@
+package com.app.SpringBootSlenieumCucumberProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSlenieumCucumberProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSlenieumCucumberProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/SpringBootSlenieumCucumberProj/SpringBootSlenieumCucumberProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SpringBootSlenieumCucumberProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSlenieumCucumberProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
